05cd5eda167d3b092f55e2112b4065b03d818d9c
Simon Zolin <s.zolin@adguard.com>
2020-02-18 14:49:50 +0300

    + clients: re-read auto-clients from /etc/hosts when SIGHUP is received

--- home/clients.go
+++ home/clients.go
@@ -107,6 +107,12 @@ func (clients *clientsContainer) Init(objects []clientObject, dhcpServer *dhcpd.
 	}
 }
 
+// Reload - reload auto-clients
+func (clients *clientsContainer) Reload() {
+	clients.addFromHostsFile()
+	clients.addFromSystemARP()
+}
+
 type clientObject struct {
 	Name                string   `yaml:"name"`
 	Tags                []string `yaml:"tags"`
@@ -187,8 +193,7 @@ func (clients *clientsContainer) WriteDiskConfig(objects *[]clientObject) {
 
 func (clients *clientsContainer) periodicUpdate() {
 	for {
-		clients.addFromHostsFile()
-		clients.addFromSystemARP()
+		clients.Reload()
 		time.Sleep(clientsUpdatePeriod)
 	}
 }
